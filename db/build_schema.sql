ca62a75db987164d918a8f9403ca1a3da3a3c114
Adam Barnhard <admbarn@gmail.com>
2014-12-05 19:03:29 -0600

    added user auth

---
+++ db/build_schema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password CHAR(60) NOT NULL,
+    avatar VARCHAR(500) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT now()
+);
+CREATE TABLE notes(
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  body TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP NOT NULL DEFAULT now(),
+  user_id INTEGER NOT NULL REFERENCES users(id)
+);
+CREATE TABLE tags(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL UNIQUE,
+  created_at TIMESTAMP NOT NULL DEFAULT now()
+);
+CREATE TABLE notes_tags(
+  note_id INTEGER NOT NULL REFERENCES notes(id),
+  tag_id INTEGER NOT NULL REFERENCES tags(id)
+);
+CREATE TABLE photos(
+  id SERIAL PRIMARY KEY,
+  url VARCHAR(500) NOT NULL,
+  note_id INTEGER NOT NULL REFERENCES notes(id)
+);
